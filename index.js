415472187165cfa912fdd613c17da2498da1cac2
scottvonscott <skeplin17@gmail.com>
2020-08-25 16:07:48 -0700

    connected front and back end

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-document.addEventListener('DOMContentLoaded', ()=> alert('hi'))
+document.addEventListener('DOMContentLoaded', () => {
+    fetchParties()
+
+})
+
+const partyList = document.getElementById('party-list')
+
+function fetchParties(){
+    fetch('http://localhost:3000/parties')
+    .then(res => res.json())
+    .then(addParties)
+}
+
+function addParties(response){
+    response.forEach( party => {
+        addPartyToDom(party)
+         })
+}
+
+function addPartyToDom(party){
+    partyList.innerHTML += `
+    <div id="party-${party.id}">
+    <li>
+    <strong class="name">${party.title}</strong>:
+    </li>
+        <button class="delete" data-id="${party.id}">Delete</button>
+        <button class="update" data-id="${party.id}">Update</button>
+        </div>`
+}
