f930e019ab2b85658cf2fc4ecde94cb887e0dd4b
scottvonscott <skeplin17@gmail.com>
2020-08-28 13:50:22 -0700

    minor css and reorg for party.js

--- src/partiesAdapter.js
+++ src/partiesAdapter.js
@@ -16,6 +16,55 @@ class PartiesAdapter{
             })
         })
     }
+
+    // CREATE
+    createParty(e){
+    e.preventDefault()
+
+    const title = document.getElementById('party-title').value
+    const mainCourse = document.getElementById('item-main-course').value
+    const snacks = document.getElementById('item-snacks').value
+    const alcoholicDrinks = document.getElementById('item-alcoholic-drinks').value
+    const music = document.getElementById('item-music').value
+    const moviesTV = document.getElementById('item-movies-tv').value
+    const games = document.getElementById('item-games').value
+    const decorations = document.getElementById('item-decorations').value
+    const costumes = document.getElementById('item-costumes').value
+
+
+    let newPartyItemsObj = [
+        {name: mainCourse, category_id: 1},
+        {name:snacks, category_id: 2},
+        {name: alcoholicDrinks, category_id: 4},
+        {name: music, category_id: 5},
+        {name: moviesTV, category_id: 6},
+        {name: games, category_id: 7},
+        {name: decorations, category_id: 8},
+        {name: costumes, category_id: 9}
+    ]
+  
+    let newPartyObj = {
+        title, 
+        items: newPartyItemsObj 
+    }
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(newPartyObj)
+    }
+
+    fetch('http://localhost:3000/parties', configObj)
+    .then(res => res.json())
+    .then(json => {
+        let party = new Party(json.data.attributes)
+        party.attachToDom()
+        
+    })
+    // partyForm.reset()
+}
     // UPDATE
     // sendPatchRequest(itemId){
     //     const price = document.getElementById(`update-price-${itemId}`).value
@@ -51,78 +100,23 @@ class PartiesAdapter{
     //     form.remove()
     // }
 
-    CREATE
-    createParty(e){
-    e.preventDefault()
-
-    const title = document.getElementById('party-title').value
-    const mainCourse = document.getElementById('item-main-course').value
-    const snacks = document.getElementById('item-snacks').value
-    const alcoholicDrinks = document.getElementById('item-alcoholic-drinks').value
-    const music = document.getElementById('item-music').value
-    const moviesTV = document.getElementById('item-movies-tv').value
-    const games = document.getElementById('item-games').value
-    const decorations = document.getElementById('item-decorations').value
-    const costumes = document.getElementById('item-costumes').value
-
-
-    let newPartyItemsObj = [
-        {name: mainCourse, category_id: 1},
-        {name:snacks, category_id: 2},
-        {name: alcoholicDrinks, category_id: 4},
-        {name: music, category_id: 5},
-        {name: moviesTV, category_id: 6},
-        {name: games, category_id: 7},
-        {name: decorations, category_id: 8},
-        {name: costumes, category_id: 9}
-    ]
-    // let newPartyItemsObj = {
-    //     mainCourse,
-    //     snacks,
-    //     alcoholicDrinks,
-    //     music,
-    //     moviesTV,
-    //     games,
-    //     decorations,
-    //     costumes
-    // }
-    let newPartyObj = {
-        title, 
-        items: newPartyItemsObj 
-    }
-    let configObj = {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        },
-        body: JSON.stringify(newPartyObj)
-    }
-
-    fetch('http://localhost:3000/parties', configObj)
-    .then(res => res.json())
-    .then(json => {
-        let party = new Party(json.data.attributes)
-        party.attachToDom()
-        
-    })
-}
-
-    // DELETE
-    // deleteItem(id){
-    //     // remove from db
-    //         let configObj = {
-    //             method: 'DELETE',
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 Accept: "application/json"
-    //             }
-    //         }
     
-    //         fetch(`http://localhost:3000/parties/${id}`, configObj)
-    //         .then(res => res.json())
-    //         .then(json => {
-    //             alert(json.message)
-    //         })
-    //     }
+
+    DELETE
+    deleteParty(id){
+        // remove from db
+            let configObj = {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                }
+            }
+    
+            fetch(`http://localhost:3000/parties/${id}`, configObj)
+            .then(res => res.json())
+            .then(json => {
+                alert(json.message)
+            })
+        }
 }
--- src/party.js
+++ src/party.js
@@ -22,6 +22,7 @@ addEventListeners(){
 
 attachToDom(){
     this.partyList.append(this.renderParty())
+    this.addEventListeners()
     
 }
 
@@ -34,7 +35,7 @@ renderParty(){
         let itemEle = document.createElement('li')
         let itemCat = document.createElement('h3')
         
-        itemCat.innerText = i.category
+        itemCat.innerText = `${i.category}:`
         itemCat.hidden = true
         itemEle.innerText = i.name
         this.element.append(itemCat)
@@ -65,28 +66,21 @@ renderParty(){
 handleListClick = (e) => {
     
     if (e.target.className === "delete"){
-        let id = e.target.dataset.id
-         this.element.remove()
-    } else if(e.target.className === 'update'){
         debugger
-         let itemId = e.target.dataset.id
-         e.target.className = "save"
-         e.target.innerText = "Save"
-         this.addUpdateFields(partyId)
-     } else if(e.target.className === 'save'){
-         let itemId = e.target.dataset.id
-         e.target.className = "update"
-         e.target.innerText = "Update"
-         itemsAdapter.sendPatchRequest(partyId)
-     }
+        let id = e.target.dataset.id
+        partiesAdapter.deleteParty(id)
+         this.element.remove()
+    } 
+    // else if(e.target.className === 'update'){
+    //      let itemId = e.target.dataset.id
+    //      e.target.className = "save"
+    //      e.target.innerText = "Save"
+    //      this.addUpdateFields(partyId)
+    //  } else if(e.target.className === 'save'){
+    //      let itemId = e.target.dataset.id
+    //      e.target.className = "update"
+    //      e.target.innerText = "Update"
+    //      itemsAdapter.sendPatchRequest(partyId)
+    //  }
  }
-
-//  updateItemOnDom({price,name,description}){
-//     // let liItem = document.querySelector(`#item-${item.id} li`)
-//     this.price = price
-//     this.description = description
-//     this.name = name
-//     this.fullRender()
-// }
-
 }
