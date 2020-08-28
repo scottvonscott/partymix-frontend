fef67400b6921ef9fe53051ff4d848cc7e03ed80
scottvonscott <skeplin17@gmail.com>
2020-08-28 16:35:52 -0700

    form sizing

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const moviesTV = document.getElementById('item-movies-tv')
 const games = document.getElementById('item-games')
 const decorations = document.getElementById('item-decorations')
 const costumes = document.getElementById('item-costumes')
+const addItemFieldButton = document.getElementById('additional-item-button')
+const categoriesDropdown = document.getElementById('categories-dropdown')
 
 const itemsAdapter = new ItemsAdapter
 const partiesAdapter = new PartiesAdapter
@@ -20,4 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
     partiesAdapter.fetchParties()
     const partyForm = document.getElementById('party-form')
     partyForm.addEventListener('submit', partiesAdapter.createParty)
+    addItemFieldButton.addEventListener('click', addItemField)
 })
+
+function addItemField(){
+   let itemField = document.getElementById('new-party-item')
+   itemField.insertAdjacentHTML('afterend', 
+   `<br><label for="categories-dropdown">Item Category:</label>
+                <select name="categories" id="categories-dropdown">
+                <option value="mainCourse">Main Courses</option>
+                <option value="snack">Snacks</option>
+                <option value="alcoholic-drinks">Alcoholic Drinks</option>
+                </select><br><input type="text" name="new-party-item" id="new-party-item">`)
+
+}
