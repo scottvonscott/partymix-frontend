66a78985158bd3310d998f18c865198f575f1529
scottvonscott <skeplin17@gmail.com>
2020-08-30 16:35:05 -0700

    wrong turn on refactor

--- src/party.js
+++ src/party.js
@@ -67,3 +67,53 @@ handleClick = (e) => {
     } 
  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// renderParty(){
+//     this.element.setAttribute('class', 'party')
+//     this.element.setAttribute('id', `Party ${this.element.id}`)
+//     this.element.innerText = this.title
+
+//     this.items.forEach(i => {
+//         let itemEle = document.createElement('li')
+//         let itemCat = document.createElement('h3')
+        
+//         itemCat.innerText = `${i.category}:`
+//         itemCat.hidden = true
+//         itemEle.innerText = i.name
+//         this.element.append(itemCat)
+//         itemCat.append(itemEle)
+//     })
+//     let deleteBtn = document.createElement('button')
+//     // let updateBtn = document.createElement('button')
+//     deleteBtn.setAttribute('class', 'delete')
+//     deleteBtn.setAttribute('data-id', this.id)
+//     deleteBtn.innerText = 'Delete'
+//     deleteBtn.hidden = true
+//     // updateBtn.setAttribute('class', 'update')
+//     // updateBtn.innerText = "Update"
+//     // updateBtn.hidden = true
+//     this.element.append(deleteBtn)
+//     this.element.addEventListener("click", expandParties(this))
+
+//     function expandParties(party){
+//         debugger
+//         let hiders = this.childNodes
+//         hiders.forEach(i => {
+//             i.hidden = !i.hidden
+//         })
+//      }
+//     return this.element
+// }
