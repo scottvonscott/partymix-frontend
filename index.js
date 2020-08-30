eb26aa49c6554f1c818a57eda1344ea24e298e7a
scottvonscott <skeplin17@gmail.com>
2020-08-30 12:09:32 -0700

    form formatting

--- index.js
+++ index.js
@@ -2,14 +2,6 @@
 // Variables
 
 const partyTitle = document.getElementById('party-title')
-// const mainCourse = document.getElementById('item-main-course')
-// const snacks = document.getElementById('item-snacks')
-// const alcoholicDrinks = document.getElementById('item-alcoholic-drinks')
-// const music = document.getElementById('item-music')
-// const moviesTV = document.getElementById('item-movies-tv')
-// const games = document.getElementById('item-games')
-// const decorations = document.getElementById('item-decorations')
-// const costumes = document.getElementById('item-costumes')
 const addItemFieldButton = document.getElementById('additional-item-button')
 const categoriesDropdown = document.getElementById('categories-dropdown')
 
@@ -23,19 +15,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const partyForm = document.getElementById('party-form')
     partyForm.addEventListener('submit', partiesAdapter.createParty)
     addItemFieldButton.addEventListener('click', addItemField)
-   //  categoriesDropdown.addEventListener("change", chooseCat);
 })
 
-// function chooseCat() {
-//    let catChoice = categoriesDropdown.value
-//    let nextInput = document.querySelector('input')
-//    nextInput.id = `item-${catChoice}`
-// }
+
 
 function addItemField(){
    let itemEnd = document.getElementById('form-anchor')
    itemEnd.insertAdjacentHTML('afterend', 
-   `<br><br><label for="categories-dropdown">Item Category:</label>
+   `<label for="categories-dropdown">Item Category:</label>
    <select class="categories-dropdown" name="categories">
    <option value="1">Main Courses</option>
    <option value="2">Snacks</option>
@@ -48,7 +35,6 @@ function addItemField(){
    <option value="8">Costumes</option>
    </select><br>
    <label for="new-item">Item Name:</label>
-   <input type="text" class="new-party-item" name="new-party-item">
-   <input type="hidden" id="form-anchor" name="form-anchor">`)
+   <input type="text" class="new-party-item" name="new-party-item"><br><br>`)
 
 }
