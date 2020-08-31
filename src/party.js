f1fb119c0fe884e2c85bad60b74220be3b132deb
scottvonscott <skeplin17@gmail.com>
2020-08-30 17:49:05 -0700

    abandoned a side nav for now

--- src/party.js
+++ src/party.js
@@ -8,9 +8,6 @@ class Party{
     this.items = items_and_categories
 
     this.element = document.createElement('div')
-    this.partyNav = partyNav
-    this.navElement = document.createElement('div')
-    this.navElement.id = `party-${this.id}`
     this.element.id = `party-${this.id}`
     this.partyList = document.getElementById('party-list')
     Party.all.push(this)
@@ -22,36 +19,21 @@ addEventListeners(){
 
 attachToDom(){
     this.partyList.append(this.renderParty())
-    this.partyNav.append(this.renderPartyNav())
     this.addEventListeners()
     
-}
-
-renderPartyNav(){
-    this.navElement.setAttribute('class', 'party')
-    this.navElement.setAttribute('id', `Party ${this.navElement.id}`)
-    this.navElement.innerText = this.title
-
-    this.navElement.addEventListener("click", expandParties(this))
-
-    function expandParties(party){
-        party.element.hidden = !party.element
-     }
-    return this.navElement
 }
 
 renderParty(){
     this.element.setAttribute('class', 'party')
     this.element.setAttribute('id', `Party ${this.element.id}`)
     this.element.innerText = this.title
-    this.element.hidden = true
 
     this.items.forEach(i => {
         let itemEle = document.createElement('li')
         let itemCat = document.createElement('h3')
         
         itemCat.innerText = `${i.category}:`
-        // itemCat.hidden = true
+        itemCat.hidden = true
         itemEle.innerText = i.name
         this.element.append(itemCat)
         itemCat.append(itemEle)
@@ -66,7 +48,14 @@ renderParty(){
     // updateBtn.innerText = "Update"
     // updateBtn.hidden = true
     this.element.append(deleteBtn)
-    
+    this.element.addEventListener("click", expandParties)
+
+    function expandParties(){
+        let hiders = this.childNodes
+        hiders.forEach(i => {
+            i.hidden = !i.hidden
+        })
+     }
     return this.element
 }
 
@@ -78,3 +67,53 @@ handleClick = (e) => {
     } 
  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// renderParty(){
+//     this.element.setAttribute('class', 'party')
+//     this.element.setAttribute('id', `Party ${this.element.id}`)
+//     this.element.innerText = this.title
+
+//     this.items.forEach(i => {
+//         let itemEle = document.createElement('li')
+//         let itemCat = document.createElement('h3')
+        
+//         itemCat.innerText = `${i.category}:`
+//         itemCat.hidden = true
+//         itemEle.innerText = i.name
+//         this.element.append(itemCat)
+//         itemCat.append(itemEle)
+//     })
+//     let deleteBtn = document.createElement('button')
+//     // let updateBtn = document.createElement('button')
+//     deleteBtn.setAttribute('class', 'delete')
+//     deleteBtn.setAttribute('data-id', this.id)
+//     deleteBtn.innerText = 'Delete'
+//     deleteBtn.hidden = true
+//     // updateBtn.setAttribute('class', 'update')
+//     // updateBtn.innerText = "Update"
+//     // updateBtn.hidden = true
+//     this.element.append(deleteBtn)
+//     this.element.addEventListener("click", expandParties(this))
+
+//     function expandParties(party){
+//         debugger
+//         let hiders = this.childNodes
+//         hiders.forEach(i => {
+//             i.hidden = !i.hidden
+//         })
+//      }
+//     return this.element
+// }
