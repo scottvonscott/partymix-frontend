47d3f82e2a4b052c7236ddb81ed6ac3b4a457f7c
scottvonscott <skeplin17@gmail.com>
2020-09-09 00:36:10 -0700

    Refactored resetfrom function, worked on Randomize

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function addItemField(){
    let itemEnd = document.getElementById('form-anchor')
-   let newHTML = `<br><div class="form-item-line"><label for="categories-dropdown" id="new-cat-label">Category:</label>
+   let newHTML = `<div class="form-item-line" id="form-item-line"><br><label for="categories-dropdown" id="new-cat-label">Category:</label>
       <select class="categories-dropdown" name="categories" id="additional-form-fields">
-      <option value="1">Main Courses</option>
-      <option value="2">Snacks</option>
-      <option value="3">Non-Alcoholic Drinks</option>
-      <option value="4">Alcoholic Drinks</option>
-      <option value="6">Movies & TV</option>
-      <option value="5">Music</option>
-      <option value="7">Games</option>
-      <option value="9">Decorations</option>
-      <option value="8">Costumes</option>
+         <option value="1">Main Courses</option>
+         <option value="2">Snacks</option>
+         <option value="3">Non-Alcoholic Drinks</option>
+         <option value="4">Alcoholic Drinks</option>
+         <option value="6">Movies & TV</option>
+         <option value="5">Music</option>
+         <option value="7">Games</option>
+         <option value="9">Decorations</option>
+         <option value="8">Costumes</option>
       </select>
       <label for="new-item" id="new-item-label">Item Name:</label>
       <input type="text" class="new-party-item "name="new-party-item" id="new-party-item"><button onclick=randomize(this)>Random</button>
@@ -41,12 +41,13 @@ function addItemField(){
 
 function randomize(e) {
    const selectedCategoryId = e.parentElement.querySelector('.categories-dropdown').value
-   const itemField = e.parentElement.querySelector('.new-party-item')
+   let itemField = e.parentElement.querySelector('.new-party-item')
    const selectedCategory = Category.all.find(cat => cat.id === parseFloat(selectedCategoryId))
    const randomizedOptions = selectedCategory.items.map(item => item.name)
    const randomChoice = getRndInteger(0, (randomizedOptions.length - 1))
-   itemField.value += randomizedOptions[randomChoice]
-   debugger
+
+   itemField.value = randomizedOptions[randomChoice]
+
 
    function getRndInteger(min, max) {
       return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -54,18 +55,26 @@ function randomize(e) {
    }
 
 function resetFormCount(){
-   while (document.getElementById('additional-form-fields')){
-   let newField = document.getElementById('additional-form-fields')
-   newField.remove()
-   }
-   while(document.getElementById('new-cat-label')){
-      let newCatLabel = document.getElementById('new-cat-label')
-      newCatLabel.previousSibling.remove()
-      newCatLabel.remove()
-   }
-   while(document.getElementById('new-item-label')){
-      let newFieldLabel = document.getElementById('new-item-label')
-     newFieldLabel.remove()
-   }
+   while (partyForm.querySelector('.form-item-line')){
+      let itemLine = partyForm.querySelector('.form-item-line')
+      itemLine.remove()
+   } addItemField()
+   
+  
+   // itemDivs.forEach(item => item.remove())
+
+   // while (document.getElementById('additional-form-fields')){
+   // let newField = document.getElementById('additional-form-fields')
+   // newField.remove()
+   // }
+   // while(document.getElementById('new-cat-label')){
+   //    let newCatLabel = document.getElementById('new-cat-label')
+   //    newCatLabel.previousSibling.remove()
+   //    newCatLabel.remove()
+   // }
+   // while(document.getElementById('new-item-label')){
+   //    let newFieldLabel = document.getElementById('new-item-label')
+   //   newFieldLabel.remove()
+   // }
 }
 
